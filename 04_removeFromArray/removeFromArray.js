743a6dcfaf67f02588fa61deaebf934b0441ad52
SoyTuPapii <168013459+SoyTuPapii@users.noreply.github.com>
2024-12-27 10:10:23 -0500

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    for (let i = 0; i < remove.length; i++) {
+        let index = array.indexOf(remove[i]);
+        while (index !== -1) {
+            array.splice(index, 1); 
+            index = array.indexOf(remove[i]); 
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
